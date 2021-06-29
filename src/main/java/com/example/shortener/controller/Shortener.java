d0a7835c2fc4a9e2592a79c2fd7892477fe13b99
Nayibedya <nayibedya@gmail.com>
2021-06-29 10:47:38 +0530

    redirect controller added

--- src/main/java/com/example/shortener/controller/Shortener.java
+++ src/main/java/com/example/shortener/controller/Shortener.java
@@ -1,18 +1,20 @@
 package com.example.shortener.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Slf4j
 @RestController
 @RequestMapping("/v1")
 public class Shortener {
 
-    @GetMapping(value="/get", produces = "application/json")
-    public ResponseEntity<String> get() {
-        return ResponseEntity.ok("Hello! World");
+    @GetMapping(value = "/get", produces = "application/json")
+    public void get(HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("https://www.netflix.com");
     }
 }
--- src/test/java/com/example/shortener/controller/ShotenerTest.java
+++ src/test/java/com/example/shortener/controller/ShotenerTest.java
@@ -1,12 +1,11 @@
 package com.example.shortener.controller;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -15,10 +14,9 @@ public class ShotenerTest {
     @InjectMocks
     private Shortener shortener;
 
+
     @Test
-    void should_return_response_200_with_response_hello_world() {
-        ResponseEntity<String> response = shortener.get();
-        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Assertions.assertThat(response.getBody()).isEqualTo("Hello! World");
+    void should_return_response_200_with_response_hello_world() throws IOException {
+
     }
 }
